f90ff132a7493c6ba1785643bb9f4c395149cc83
RichLovesCalculus <RichLovesCalculus@gmail.com>
2025-11-04 16:17:28 -0500

    Decoupled UserRetrievalTest.java from ApiBaseTest.java

--- RichAutomationDemo/src/test/java/tests/api/UserRetrievalTests.java
+++ RichAutomationDemo/src/test/java/tests/api/UserRetrievalTests.java
@@ -1,19 +1,37 @@
 package tests.api;
 
-import base.ApiBaseTest;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import io.restassured.response.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+import reporting.ExtentManager;
+import utils.APIClient;
 import utils.ApiResponseHandler;
 import utils.TestLogger;
 
-public class UserRetrievalTests extends ApiBaseTest {
+public class UserRetrievalTests {
+    private final ExtentReports extent = ExtentManager.getInstance();
     private static final Logger logger = LogManager.getLogger(UserRetrievalTests.class);
 
     @Test
     public void testGetUserReturns200() {
-        test = TestLogger.startTest("Sending GET request and verifying 200 response", extent, logger);
-        response = requestSpec.get("/public/v2/users/8222292");
+        ExtentTest test = TestLogger.startTest("Sending GET request and verifying 200 response", extent, logger);
+        Response response = APIClient.getUser(8222278);
         ApiResponseHandler.handleResponse(response, test, logger);
+    }
+
+    @Test
+    public void testGetUsersReturns200() {
+        ExtentTest test = TestLogger.startTest("Sending GET request and verifying 200 response", extent, logger);
+        Response response = APIClient.getUsers();
+        ApiResponseHandler.handleResponse(response, test, logger);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        extent.flush();
     }
 }
--- RichAutomationDemo/src/test/java/utils/APIClient.java
+++ RichAutomationDemo/src/test/java/utils/APIClient.java
@@ -3,21 +3,45 @@ package utils;
 import io.restassured.response.Response;
 import org.h2.util.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static io.restassured.RestAssured.given;
 
 public class APIClient {
+    private static final String authToken = loadToken();
+
+    private static String loadToken() {
+        Properties props = new Properties();
+        try (InputStream input = APIClient.class.getClassLoader().getResourceAsStream("secrets.properties")) {
+            props.load(input);
+            return props.getProperty("oauth2.token");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load OAuth2 token", e);
+        }
+    }
+
     public static Response getUser(int id) {
         return given()
                 .baseUri("https://gorest.co.in/public/v2")
-                .auth().oauth2("your_token")
+                .auth().oauth2(authToken)
                 .when()
                 .get("/users/" + id);
+    }
+
+    public static Response getUsers() {
+        return given()
+                .baseUri("https://gorest.co.in/public/v2")
+                .auth().oauth2(authToken)
+                .when()
+                .get("/users/");
     }
 
     public static Response createUser(JSONObject payload) {
         return given()
                 .baseUri("https://gorest.co.in/public/v2")
-                .auth().oauth2("your_token")
+                .auth().oauth2(authToken)
                 .header("Content-Type", "application/json")
                 .body(payload.toString())
                 .when()
